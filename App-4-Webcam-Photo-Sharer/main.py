2db31f918cde73aa90cd843ba35ed4e7d871918d
Enea Jorgji <jorgji.enea@gmail.cm>
2021-08-22 22:29:38 +0200

    edited method create_link()

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
 import time
 
 from filesharer import FileSharer
@@ -10,25 +11,35 @@ Builder.load_file("frontend.kv")
 
 class CameraScreen(Screen):
     def start(self):
+        """Starts camera and changes Button text"""
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
+        """Stops camera and changes Button text"""
         self.ids.camera.play = False
         self.ids.camera_button.text = "Start Camera"
         self.ids.camera.texture = None
 
     def capture(self):
+        """Creates a filename with the current time, captures and
+        saves a photo image under that filepath"""
         current_time = time.strftime("%Y%m%d-%H%M%S")
-        filepath = f"files/{current_time}.png"
-        self.ids.camera.export_to_png(filepath)
+        self.filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(self.filepath)
         self.manager.current = "image_screen"
-        self.manager.current_screen.ids.img.source = filepath
+        self.manager.current_screen.ids.img.source = self.filepath
 
 
 class ImageScreen(Screen):
-    pass
+    def create_link(self):
+        """Accesses the photo filepat, uploads it to the web, and inserts
+        the link in the Label widget"""
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+        filesharer = FileSharer(filepath=file_path)
+        url = filesharer.share()
+        self.ids.link.text = url
 
 
 class RootWidget(ScreenManager):
