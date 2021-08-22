d5e8cbd71ef72e5dfadf4d06b4f6ae5de04984de
Enea Jorgji <jorgji.enea@gmail.cm>
2021-08-22 19:14:58 +0200

    added method start, stop and capture to class CameraScreen

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -2,7 +2,27 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+from filesharer import FileSharer
+
 Builder.load_file("frontend.kv")
+
+
+class CameraScree():
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
+
+
+class
+
+
+class ImageScreen():
+    pass
 
 
 class RootWidget(ScreenManager):
