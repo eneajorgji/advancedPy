3c6e00704aa55ae9e6371beadac2afab4c33a023
Enea Jorgji <jorgji.enea@gmail.cm>
2021-08-22 22:04:25 +0200

    edited method capture

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import time
 
 from filesharer import FileSharer
 
@@ -10,12 +11,20 @@ Builder.load_file("frontend.kv")
 class CameraScreen(Screen):
     def start(self):
         self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
         self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        current_time = time.strftime("%Y%m%d-%H%M%S")
+        filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(filepath)
+        self.manager.current = "image_screen"
+        self.manager.current_screen.ids.img.source = filepath
 
 
 class ImageScreen(Screen):
