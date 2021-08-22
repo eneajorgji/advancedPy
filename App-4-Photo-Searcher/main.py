f889e9791ae470305c90ccdeb11758d0bab4b4cc
Enea Jorgji <jorgji.enea@gmail.cm>
2021-08-22 12:27:21 +0200

    edited class FirstScreen

--- App-4-Photo-Searcher/main.py
+++ App-4-Photo-Searcher/main.py
@@ -2,12 +2,29 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+import wikipedia
+import requests
+
 Builder.load_file("frontend.kv")
 
 
 class FirstScreen(Screen):
     def search_image(self):
-        self.manager.current_screen.ids.img.source = "files/sunny.jpg"
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.user_query
+
+        # Get wikipedia page and the first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+
+        # Download the image
+        req = requests.get(image_link)
+        image_path = "files/image.jpg"
+        with open(image_path, "wb") as file:
+            file.write(req.content)
+
+        # Set the image in the Image Widget
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
